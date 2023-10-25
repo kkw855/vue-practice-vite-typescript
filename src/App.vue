7b05930f0dfe933695e21c0b66af2bb1ccba0b4b
kkw855 <kkw855@gmail.com>
2023-10-25 15:09:50 +0900

    ✨ feat: CSS Module 사용하기

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import styles from './assets/example.module.css'
+
+console.log(styles)
+</script>
 
 <template>
   <h1>Hello Vue 3 + Vite</h1>
@@ -6,6 +10,7 @@
   <div id="post">
     <p>PostCSS</p>
   </div>
+  <p :class="styles.example">Hello CSS Modules</p>
 </template>
 
 <style>
